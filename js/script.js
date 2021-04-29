17ee9bc0c14b06866115b48cc7544555506df003
H0llywood92 <manuele.volponi@gmail.com>
2021-04-29 15:08:17 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -3,15 +3,36 @@ var app = new Vue (
         el: '#root',
 
         data: {
-
+            userSearch: '',
+            resultArray: []
         },
 
         methods: {
 
+            searchEnter() {
+                axios
+                .get('https://api.themoviedb.org/3/search/multi', {
+                params: {
+                api_key: 'a21d6a53ae3ba4432e6ec0b5967e1ce3',
+                query: this.userSearch
+                }
+                })
+                .then( (response) => {
+            
+                const resultData = response.data.results;
+            
+                this.resultArray = resultData;
+                })
+
+            },
+
+            
         },
 
         mounted() {
-            
+
+               
+
         }
     }
 ) 
