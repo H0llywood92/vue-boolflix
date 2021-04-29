971c02ddb710f7243dffd8214c2b0e545368780f
H0llywood92 <manuele.volponi@gmail.com>
2021-04-29 17:48:56 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -9,23 +9,36 @@ var app = new Vue (
 
         methods: {
 
+            //Converter vote in number from 1 to 5
+            convertVote(data){
+
+                let normalAverage = data.vote_average;
+
+                let convertAverage = Math.ceil(normalAverage / 2);
+
+
+                return convertAverage
+            },
+
+
             searchEnter() {
                 axios
-                .get('https://api.themoviedb.org/3/search/multi', {
-                params: {
-                api_key: 'a21d6a53ae3ba4432e6ec0b5967e1ce3',
-                query: this.userSearch,
-                language: 'it'
-                }
-                })
-                .then( (response) => {
+                    .get('https://api.themoviedb.org/3/search/multi', {
+                        params: {
+                        api_key: 'a21d6a53ae3ba4432e6ec0b5967e1ce3',
+                        query: this.userSearch,
+                        language: 'it'
+                    }
+                    })
+                    .then( (response) => {
             
-                const resultData = response.data.results;
+                        const resultData = response.data.results;
             
-                this.resultArray = resultData;
-                })
-
+                        this.resultArray = resultData;
+                    })
+               
             },
+            
 
             
         },
